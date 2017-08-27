38c93c297f79449059b5fdb1d83c6eeb7a849cea
ProkopHapala <ProkopHapala@gmail.com>
2017-08-27 17:33:36 +0200

    added threejs_examples

---
+++ threejs_examples/files.js
@@ -0,0 +1,27 @@
+var files = {
+	"primitives": [
+		"webgl_geometries",
+		"webgl_geometry_extrude_splines",
+		"webgl_geometry_spline_editor",
+	],
+	"interactive": [
+		"webgl_interactive_buffergeometry",
+		"webgl_interactive_cubes",
+		"webgl_interactive_cubes_gpu",
+		"webgl_interactive_cubes_ortho",
+		"webgl_interactive_draggablecubes",
+		"webgl_interactive_instances_gpu",
+		"webgl_interactive_lines",
+		"webgl_interactive_points",
+		"webgl_interactive_raycasting_points",
+		"webgl_interactive_voxelpainter",
+	],
+	"advanced": [
+		"webgl_clipping",
+		"webgl_camera_logarithmicdepthbuffer",
+		"webgl_gpgpu_water",
+		"webgl_shaders_ocean2",
+		"webgl_geometry_terrain_raycast",
+	]
+};
+
