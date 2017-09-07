02eb299901e28eb00b199b9d35aafcfa051f8c0b
ProkopHapala <ProkopHapala@gmail.com>
2017-09-07 17:29:12 +0200

    performance/Nbody rawNumber version takes just 6ns

--- performance/Nbody/Nbody.js
+++ performance/Nbody/Nbody.js
@@ -3,7 +3,6 @@
 
 "use strict";
 
-var R3SAFE = 1e+2;
 var p0     = [250.0,250.0];
 
 function set ( f      , out ){ out[0]=f;           out[1]=f; };
@@ -18,11 +17,13 @@ function move( poss, vels, dt ){
     var n = poss.length;
     var d  = [0.0,0.0];
     var f  = [0.0,0.0];
+    var K  = -0.0001;
+    var R3SAFE = 1e+2;
     for( var i=0; i<n; i++ ){
         var pi = poss[i]; 
         //set(0.0,f);
         sub( pi, p0, d);
-        mulf( d,-0.0001, f);
+        mulf( d,K, f);
         for( var j=0; j<n; j++ ){
             if( i==j ) continue;
             var pj = poss[j];
@@ -32,9 +33,44 @@ function move( poss, vels, dt ){
             accf( f, d, fr );
             //console.log( i, j, d, f );
         }
+        //console.log( f );
         var vel = vels[i];
         accf( vel, f,  dt );
         accf( pi, vel, dt );
+    }
+}
+
+function move_rawNum( poss, vels, dt ){
+    var n = poss.length;
+    var p0_x = p0[0];
+    var p0_y = p0[1];
+    var K    = -0.0001;
+    var R3SAFE = 1e+2;
+    for( var i=0; i<n; i++ ){
+        var pi = poss[i];
+        var pi_x = pi[0];
+        var pi_y = pi[1];
+        var fx = (pi_x - p0_x)*K;
+        var fy = (pi_y - p0_y)*K;
+        for( var j=0; j<n; j++ ){
+            if( i==j ) continue;
+            var pj   = poss[j];
+            var pj_x = pj[0];
+            var pj_y = pj[1];
+            var dx   = pi_x - pj_x;
+            var dy   = pi_y - pj_y;
+            var r2   = dx*dx + dy*dy;
+            var fr   = -1.0/( r2*Math.sqrt(r2) + R3SAFE);
+            fx += dx*fr;
+            fy += dy*fr;
+            //console.log( i, j, dx,dy, fx, fy );
+        }
+        //console.log( fx, fy );
+        var vel = vels[i];
+        var vx = vel[0] + fx*dt;  vels[i][0] = vx;
+        var vy = vel[1] + fy*dt;  vels[i][1] = vy;
+        poss[i][0] += vx*dt;
+        poss[i][1] += vy*dt;
     }
 }
 
@@ -69,7 +105,8 @@ function updateScene(){
     var perFrame = 10;
     var t0 = performance.now();
     for( var itr=0; itr<perFrame; itr++ ){
-        move( poss, vels, dt );
+        //move( poss, vels, dt );
+        move_rawNum( poss, vels, dt );
     }
     var T = performance.now() - t0;
     console.log( "updateScene", T, " [ms] ", 1e+6*T/(poss.length*poss.length*perFrame), " [ns/op]" );
@@ -92,6 +129,9 @@ window.onload = function () {
         vels.push( vel );
     }
     drawPoints(ctx, poss );
+    //console.log("==============");
     //move( poss, vels, dt );
+    //console.log("==============");
+    //move_rawNum( poss, vels, dt );
     updateScene();
 }
