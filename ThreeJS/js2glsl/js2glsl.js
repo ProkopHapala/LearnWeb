a9c4ef4dae23e304561952b508d6aef895a36b54
ProkopHapala <ProkopHapala@gmail.com>
2018-06-02 18:15:07 +0200

    set glslScreen facing camera and proper cilinder orientation

--- ThreeJS/js2glsl/js2glsl.js
+++ ThreeJS/js2glsl/js2glsl.js
@@ -5,6 +5,15 @@ var scene_root = [];
 
 var DEFAULTS = { nCircle: 8 };
 
+var tmps = {
+	v_pos:   new THREE.Vector3(),
+	v_up:   new THREE.Vector3(),
+	v_fw:   new THREE.Vector3(),
+	v_left: new THREE.Vector3(),
+	m4_1 : new THREE.Matrix4(),
+};
+
+
 function makeDownloadableFile(text, id, fname) {
 	type = 'text/plain';
 	var a = document.getElementById(id);
@@ -39,12 +48,32 @@ class Cylinder {
 	toThreeJS(material) {
 		//console.log( "toThreeJS : R=" +this.R + " pos= "+this.pos );
 		let dp = new THREE.Vector3();
-		dp.subVectors(new THREE.Vector3(this.p2[0], this.p2[1], this.p2[2]), new THREE.Vector3(this.p1[0], this.p1[1], this.p1[2]));
+		//let p1 = new THREE.Vector3(this.p2[0], this.p2[1], this.p2[2]);
+		//let p2 = new THREE.Vector3(this.p1[0], this.p1[1], this.p1[2]);
+		let p1 = new THREE.Vector3().fromArray(this.p1);
+		let p2 = new THREE.Vector3().fromArray(this.p2);
+		dp.subVectors(p2,p1);
 		let L = dp.length();
 		//console.log( "L= "+L+"  dp "+dp );
 		let object = new THREE.Mesh(new THREE.CylinderGeometry(this.R, this.R, L, DEFAULTS.nCircle), material);
-		object.lookAt(dp);
-		object.position.set(this.p1[0], this.p1[1], this.p1[2]);
+		//object.lookAt(dp);
+
+		object.matrixAutoUpdate = false;
+		object.matrix.setDirUp(dp);
+		object.matrix.swap(1,2);
+		object.quaternion.setFromRotationMatrix( object.matrix );
+		console.log(object.matrix.toString()  );
+
+		p1.addScaledVector (dp,0.5);
+		//object.position.copy(p1);
+		object.matrix.setPosition ( p1 );
+
+		//object.matrixAutoUpdate = true;  // THIS BREAKS EVERYTHING
+
+		console.log("p1=",p1);
+		console.log("p2=",p2);
+		console.log("dp=",dp);
+
 		return object;
 	}
 	toGlsl() {
--- js_libs/ThreeJS/ThreeJS_aux.js
+++ js_libs/ThreeJS/ThreeJS_aux.js
@@ -5,10 +5,11 @@ THREE.Screen = function ( container ) {
     //var container, renderer, camera, controls, scene;
     this.renderer  = initRendered(container);
     this.camera    = new THREE.PerspectiveCamera( 60, _this.aspectRatio, 1, 10000 );  _this.camera.position.z = 500;
+
     this.controls  = initControls(_this.camera);
     this.materials = initMaterials();
     this.scene     = initScene();
-
+    
     function initRendered(container){
         // renderer
         let renderer = new THREE.WebGLRenderer( { antialias: false } );
@@ -37,7 +38,38 @@ THREE.Screen = function ( container ) {
         controls.addEventListener( 'change', function(){_this.render(); } ); // Trick described here  https://stackoverflow.com/questions/20279484/how-to-access-the-correct-this-inside-a-callback
         return controls;
     }
+
+    this.initShaderScreen = function( src_vert, src_frag ) {
+
+        //basicShader = THREE.ShaderLib['normal'];
+        let uniforms = {
+            time: { value: 1.0 },
+            resolution: { value: new THREE.Vector2() },
+            camMat: { value: new THREE.Matrix3() }
+        };
+        uniforms.resolution.value.x = _this.renderer.domElement.width;
+        uniforms.resolution.value.y = _this.renderer.domElement.height;
+
+        let matGLSL = new THREE.ShaderMaterial({
+            uniforms:       uniforms,
+            vertexShader:   src_vert,
+            fragmentShader: src_frag,
+        });
+
+        try {
+            var mesh = _this.scene.getObjectByName("glslScreen");
+            _this.scene.remove(mesh);
+        } catch (err) {
+            console.log(" updateShader cannot remove Mesh1");
+        };
     
+        //mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry(100, 100), matGLSL );
+        mesh = new THREE.Mesh( new THREE.PlaneGeometry(10000, 10000), matGLSL );
+        mesh.name = "glslScreen";
+        _this.scene.add(mesh);
+    }
+
+
     function initMaterials(){
         return {
             wire    : new THREE.MeshBasicMaterial( { color: 0x808080, wireframe: true } ),
@@ -65,18 +97,6 @@ THREE.Screen = function ( container ) {
 
         return scene;
     }
-
-    /*
-    // This does not work
-    function animate() {
-        requestAnimationFrame( _this.animate );
-        _this.controls.update();
-    }
-
-    function render() {
-        _this.renderer.render( _this.scene, _this.camera );
-    }
-    */
     
     this.animate = ( function () {
         return function animate(){
@@ -85,8 +105,13 @@ THREE.Screen = function ( container ) {
         }
     }() );
 
+    this.userUpdate = function(self){};
+
     this.render = ( function() {
-        return function render(){ _this.renderer.render( _this.scene, _this.camera ); };
+        return function render(){ 
+                _this.userUpdate(_this); 
+                _this.renderer.render( _this.scene, _this.camera );
+            };
     }() );
 
 }
@@ -94,11 +119,65 @@ THREE.Screen = function ( container ) {
 // NOTE : we can implement new functionality to any existing class this way
 
 function IMPLEMENT_Vector3_FUNCS () {
+
     THREE.Vector3.prototype.prokop_op = function (){
         this.x = 3.14159265359;
         this.y = 2.71828182846;
         this.z = 1.61803398874;
     }
+
+    THREE.Matrix4.prototype.swap = function(i,j){
+        let i3 = i*4;
+        let j3 = j*4;
+        let t;
+        let te = this.elements;
+        t=te[i3  ];  te[i3  ]=te[j3  ];  te[j3  ]=t;
+        t=te[i3+1];  te[i3+1]=te[j3+1];  te[j3+1]=t;
+        t=te[i3+2];  te[i3+2]=te[j3+2];  te[j3+2]=t;
+    }
+
+
+    THREE.Matrix4.prototype.toString= function(){
+        let te = this.elements;
+        return  "[["+te[0]+","+te[1]+","+te[2]+","+te[3]+"]\n"
+               +" ["+te[4]+","+te[5]+","+te[6]+","+te[7]+"]\n"
+               +" ["+te[8]+","+te[9]+","+te[10]+","+te[11]+"]\n"
+               +" ["+te[12]+","+te[13]+","+te[14]+","+te[15]+"]]\n";
+    }
+
+    THREE.Matrix4.prototype.setDirUp = ( function(){ 
+        var dir_ = new THREE.Vector3();
+        var up_  = new THREE.Vector3();
+        var side = new THREE.Vector3();
+        return function setDirUp(dir,up){
+            /*
+            // make orthonormal rotation matrix c=dir; b=(up-<b|c>c)/|b|; a=(c x b)/|a|;
+            c.set(dir);
+            //c.normalize(); // we assume dir is already normalized
+            b.set(up);
+            b.add_mul( c, -b.dot(c) );   //
+            b.normalize();
+            a.set_cross(b,c);
+            */
+            dir_.copy(dir);
+            dir_.normalize();
+            if(up){ up_.copy(up); } else { up_.set(0.0,1.0,0.0); }
+            let cdir = dir_.dot(up_);
+            up_.addScaledVector ( dir_, -cdir );
+            up_.normalize();
+            side.crossVectors   ( up_, dir_ );
+
+            console.log( "dir_", dir_ );
+            console.log( "up_ ", up_ );
+            console.log( "side", side );
+            
+            let te = this.elements;
+            te[ 0 ] = side.x;  te[ 4 ] = up_.x;  te[ 8 ] = dir_.x;
+            te[ 1 ] = side.y;  te[ 5 ] = up_.y;  te[ 9 ] = dir_.y;
+            te[ 2 ] = side.z;  te[ 6 ] = up_.z;  te[10 ] = dir_.z;
+        }
+    }() );
+
 }
 
 // from here: https://stackoverflow.com/questions/33152132/three-js-collada-whats-the-proper-way-to-dispose-and-release-memory-garbag
